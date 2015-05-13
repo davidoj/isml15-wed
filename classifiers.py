80d26366201c1b0bbae14203a679ba234ee10527
davidoj <orion.tjungarryi@gmail.com>
2015-05-13 18:41:07 +1000

    Implemented a basic linear regression classifier.

---
+++ classifiers.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+
+def reg_classifier(data,labels):
+    """
+    Calculates a vector of weights for classifying data
+    Args: data - MxN matrix (including bias)
+    labels - length M vector
+    Returns a length N vector of weights for classifying the data
+    """
+   
+    assert data.shape[0]==labels.shape[0], "labels not aligned with data"
+
+    pinv = np.linalg.pinv(data)
+    w = pinv.dot(labels)
+    
+    return w
+
+def load_data(filename):
+    """
+    Reads filename, returns matrix of data with bias column
+    and a vector of labels
+    """
+    data = pd.read_csv(filename,header=0)
+    labels = np.array(data['sentiment'])
+    data = data.drop('id',axis=1)
+    data['bias']=np.ones(data.shape[0])
+    
+    return np.array(data), labels
